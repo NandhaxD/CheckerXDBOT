2d21e09f24cd303a26ac2a9cf896edff478edf4c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-01 23:08:14 +0530

    Update untils.py

--- untils.py
+++ untils.py
@@ -1,14 +1,41 @@
 
+
+
+from datetime import datetime
+
 import requests
 import random
+import re
+import json
 
 
 
-
-class checker:
-
-    
+class Checker:
+  
     @staticmethod
-    def generator():
-         return 'ok'
+    def generator(bin: int, limit : int = 1):
+       base_url = "https://namsogen.org/ajax.php"
+              
+       form_data = {
+          "type": "3",
+          "bin": bin,
+          "date": "on",
+          "csv": "on",
+          "number": limit,
+          "format": "json"
+          }
+       response = requests.post(base_url, data=form_data)
+       if response.status_code == 200:
+            data = response.text
+            clean_response_text = re.sub(r'[^\x20-\x7E]+', '', data)
+            clean_response_text = re.sub(r',\s*}', '}', clean_response_text)
+            clean_response_text = re.sub(r',\s*]', ']', clean_response_text)
+            data = json.loads(clean_response_text)
+            return data
+       else:
+            return []
+            
+
+ 
+         
        
